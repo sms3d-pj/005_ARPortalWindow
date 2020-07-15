6e2434a0ab548fd54a83e50964b360d709287b70
mattatz <masatatsu.nakamura@gmail.com>
2020-07-15 11:35:23 +0900

    Create ARImageTest scene.

---
+++ Assets/SampleMovies/Scripts/ARImageTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARImageTest : MonoBehaviour
+{
+    [SerializeField] protected ARCameraBackground arCamBg;
+    [SerializeField] protected RenderTexture image;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Graphics.Blit(null, image, arCamBg.material);
+    }
+
+    protected void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(10, 10, 512, 512), image);
+    }
+
+}
